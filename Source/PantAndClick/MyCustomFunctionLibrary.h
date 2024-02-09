09043cec8f7cfe661f16474f1c1045dc415e746f
PantDev <111689785+PantDev@users.noreply.github.com>
2024-02-09 07:52:19 -0500

    Mega Update
    Overdue push from new PC. Paranoid about losing progress so here's a messy push.

---
+++ Source/PantAndClick/MyCustomFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyCustomFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANTANDCLICK_API UMyCustomFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
